9e98a8cf2ca1282f4096c4c68ff634b6e7d0fed2
Dorian <SxMShaDoW@users.noreply.github.com>
2025-05-26 22:23:23 +0000

    fix crash

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,22 +1,15 @@
-
 //go:build wasm
 // +build wasm
 
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/base64"
-	"log"
-	"os"
-	"io"
-	"syscall/js"
-	
-	"google.golang.org/protobuf/proto"
-	v1 "github.com/vultisig/commondata/go/vultisig/vault/v1"
-	"main/pkg/types"
-	"main/pkg/shared"
+    "log"
+    "syscall/js"
+    "os"
+    "io"
+    "main/pkg/types"
+    "main/pkg/shared"
 )
 
 func main() {
@@ -66,107 +59,6 @@ func main() {
         return result
     }))
 
-    // Add vault decryption function
-    js.Global().Set("DecryptVault", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-        if len(args) < 2 {
-            return "Error: DecryptVault requires base64 vault data and password"
-        }
-
-        base64Data := args[0].String()
-        password := args[1].String()
-
-        // Decode base64
-        vaultData, err := base64.StdEncoding.DecodeString(base64Data)
-        if err != nil {
-            return "Error decoding base64: " + err.Error()
-        }
-
-        // Decrypt with AES-GCM
-        decryptedData, err := decryptWithAesGcm(vaultData, password)
-        if err != nil {
-            return "Error decrypting vault: " + err.Error()
-        }
-
-        // Parse protobuf
-        vault := &v1.Vault{}
-        if err := proto.Unmarshal(decryptedData, vault); err != nil {
-            return "Error parsing protobuf: " + err.Error()
-        }
-
-        // Return formatted vault info
-        return formatVaultInfo(vault)
-    }))
-
     log.Println("WASM initialization complete, waiting for JS calls...")
     <-c
-}
-
-func decryptWithAesGcm(data []byte, password string) ([]byte, error) {
-    key := []byte(password)
-    if len(key) < 32 {
-        // Pad key to 32 bytes
-        padded := make([]byte, 32)
-        copy(padded, key)
-        key = padded
-    } else if len(key) > 32 {
-        key = key[:32]
-    }
-
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return nil, err
-    }
-
-    gcm, err := cipher.NewGCM(block)
-    if err != nil {
-        return nil, err
-    }
-
-    nonceSize := gcm.NonceSize()
-    if len(data) < nonceSize {
-        return nil, err
-    }
-
-    nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-    plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-    if err != nil {
-        return nil, err
-    }
-
-    return plaintext, nil
-}
-
-func formatVaultInfo(vault *v1.Vault) map[string]interface{} {
-	keyShares := make([]map[string]interface{}, len(vault.KeyShares))
-	for i, ks := range vault.KeyShares {
-		keyShares[i] = map[string]interface{}{
-			"publicKey": ks.PublicKey,
-			"keyshare":  ks.Keyshare[:min(100, len(ks.Keyshare))] + "...", // Truncate for display
-		}
-	}
-
-	// Determine scheme type based on vault characteristics
-	schemeType := "GG20" // Default
-	if vault.ResharePrefix != "" {
-		schemeType = "DKLS"
-	}
-
-	return map[string]interface{}{
-		"name":            vault.Name,
-		"publicKeyEcdsa":  vault.PublicKeyEcdsa,
-		"publicKeyEddsa":  vault.PublicKeyEddsa,
-		"signers":         vault.Signers,
-		"keyShares":       keyShares,
-		"localPartyId":    vault.LocalPartyId,
-		"resharePrefix":   vault.ResharePrefix,
-		"schemeType":      schemeType,
-		"type":           "vault",
-	}
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
